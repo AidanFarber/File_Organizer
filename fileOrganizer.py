de6c94c318214277f1f66a9d656b4dcfaec7300b
Aidan Farber <farberai31@gmail.com>
2020-04-02 13:29:20 -0400

    4/2 1:30pm Revision

---
+++ fileOrganizer.py
@@ -0,0 +1,15 @@
+'''
+    - Create a script that monitors the desktop
+    - Any time there is an update while the script is running, move files accordingly
+        ex. if it's a .txt file, send it to the "text" subfolder
+    - Script must run in the background
+    - We want to leave apps and shortcuts as is
+'''
+
+import os
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
